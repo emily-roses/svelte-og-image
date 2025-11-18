a170d599d032e5584606f84cc95af5f24ed5b03d
Emily Rose <hello@fauxtrots.com>
2025-11-18 11:31:34 -0500

    Fix build

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			split: true,
-			runtime: 'edge'
-		})
+		adapter: adapter()
 	}
 };
 
